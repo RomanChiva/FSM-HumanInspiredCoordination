de257277b044f3e34dca9431af8df9f82804b565
RomanChiva <rchivag@gmail.com>
2023-07-01 19:38:51 -0400

    Chnged Branch Name

---
+++ tests.py
@@ -0,0 +1,15 @@
+import numpy as np
+import matplotlib.pylab as plt
+import mistree as mist
+import scipy as sp
+import random
+
+from scipy.spatial import ConvexHull, convex_hull_plot_2d
+
+import numpy as np
+
+a = np.array([1,2])
+print(type(a))
+
+
+
